9a12fcde930ca06c6423daef80d4171b0dc26f4c
muhaddisumer <muhaddis.umer003@gmail.com>
2023-01-16 01:16:22 +0500

    Cros Issue

--- src/store/axios.js
+++ src/store/axios.js
@@ -2,12 +2,6 @@ import axios from 'axios';
 import { ApiUrl, token } from './config';
 
 axios.defaults.baseURL = ApiUrl;
-axios.defaults.headers.common['Access-Control-Max-Age'] = "1800";
-axios.defaults.headers.common['Access-Control-Allow-Host'] = "*";
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = "*";
-axios.defaults.headers.common['Access-Control-Allow-Credentials'] = "true";
-axios.defaults.headers.common['Access-Control-Allow-Headers'] = "Origin, X-Requested-With, Content-Type, Accept";
-axios.defaults.headers.common['Access-Control-Allow-Methods'] = "PUT, POST, GET, DELETE, PATCH, OPTIONS";
 axios.defaults.headers.common['Content-Type'] = "application/json;charset=utf-8";
 
 if (token) axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
